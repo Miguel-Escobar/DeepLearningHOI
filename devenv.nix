4f080fa07710900866c50a05086fdc8ceb4d9419
Miguel <77211681+Miguel-Escobar@users.noreply.github.com>
2025-02-08 02:43:58 -0300

    Initial devenv setup

---
+++ devenv.nix
@@ -0,0 +1,52 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.python312Packages.torch
+    pkgs.python312Packages.torchvision 
+  ];
+
+  # https://devenv.sh/languages/
+  languages.python.enable = true;
+  languages.python.version = "3.12.8";
+  languages.python.venv.enable = true;
+  languages.python.venv.requirements = ./requirements.txt;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    zsh
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
